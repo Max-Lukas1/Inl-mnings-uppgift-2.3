2b6e676067a3e897650edc64f60987448f7f78b2
majo06002 <Max-Lukas@outlook.com>
2023-09-20 10:21:02 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inlämningsuppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du köra?");
+            string dagar = Console.ReadLine();
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            string Kilometer = Console.ReadLine();
+            int Kilometers = int.Parse(Kilometer);
+            int dagars = int.Parse(dagar);
+            int svar = dagars * 500 + 300 + Kilometers;
+            Console.WriteLine(svar + " kr");
+        }
+    }
+}
+
